ef0247a38519cf07d56e616ae5a68ddf595dadd0
Abdullah-Milon <almilonabdullah80@gmail.com>
2023-05-02 23:41:48 +0600

    set up signin and sign out has completed

--- src/Provider/AuthProviders.jsx
+++ src/Provider/AuthProviders.jsx
@@ -1,37 +1,43 @@
 import React, { createContext, useEffect, useState } from 'react';
-import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import app from '../firebase/firebase.config';
 
 export const AuthContext = createContext(null);
 
 const auth = getAuth(app)
 
-const AuthProviders = ({children}) => {
+const AuthProviders = ({ children }) => {
 
     const [user, setUser] = useState(null);
 
-    const createUser = (email, password) =>{
+    const createUser = (email, password) => {
         return createUserWithEmailAndPassword(auth, email, password)
     }
 
-    const signIn = (email, password) =>{
+    const signIn = (email, password) => {
         return signInWithEmailAndPassword(auth, email, password)
     }
 
-    useEffect(() =>{
-       const unsubscribe = onAuthStateChanged(auth, currentUser =>{
+    const logOut = ()  =>{
+       return signOut(auth)
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
             console.log('auth state change', currentUser)
+            setUser(currentUser);
         });
 
-        return() => {
+        return () => {
             unsubscribe()
         }
-    },[])
+    }, [])
 
     const authInfo = {
         user,
         createUser,
         signIn,
+        logOut,
     }
     // const user = {displayName: 'Sagor Nodi'}
     return (
